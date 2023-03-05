78b6e0d60e3dde51851b8e81fa6b77a5dfed662b
tindao02 <timdao02@hotmail.com>
2023-03-05 17:27:09 -0300

    propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import { Post } from './assets/Post'
 export function App() {
   return (
     <div>
-      <h1>APP</h1>
-      <Post></Post>
-      <Post></Post>
-      <Post></Post>
-      <Post></Post>
-      <Post></Post>
+      <Post 
+        author="Diego Fernandes"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores autem dolore mollitia?"
+      />
+      <Post 
+        author="Manoel Ozeias"
+        content="Novo post"
+      />
     </div>
   )
 }
+
+
--- src/assets/Post.jsx
+++ src/assets/Post.jsx
@@ -1,3 +1,8 @@
-export function Post() {
-    return <p>Primeiro Component</p>
+export function Post(props) {
+    return (
+        <div>
+            <strong>{props.author}</strong>
+            <p>{props.content}</p>
+        </div>
+    )
 }
