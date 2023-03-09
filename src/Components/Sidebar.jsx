70d9af72074dd739c364c8fa4eb45ac6111814f1
tindao02 <timdao02@hotmail.com>
2023-03-08 23:25:39 -0300

    Finalizando sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from '@phosphor-icons/react'
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,12 +11,21 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <strong>Maria Julia</strong>
+                <img 
+                    src="https://images.unsplash.com/photo-1517598024396-46c53fb391a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=150&q=30" 
+                    alt="Foto perfil"
+                    className={styles.avatar}
+                />
+
+                <strong>Flávio Batista</strong>
                 <span>Frontend</span>
             </div>
 
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
             </footer>
 
         </aside>
